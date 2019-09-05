25c9c21d8e82b432b66b300cd66edebc99895811
EricNY <ericabt@gmail.com>
2019-09-05 12:38:18 -0400

    Updates

---
+++ precache-manifest.edd7f65ddcbd9ae6ef7f5e99326e524a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10029f8fa88bc551dac184876d01e862",
+    "url": "/Whale-Finder/index.html"
+  },
+  {
+    "revision": "47dcd11188febd5aba8e",
+    "url": "/Whale-Finder/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "db99e0b14166723c6d78",
+    "url": "/Whale-Finder/static/css/main.e86ca9f3.chunk.css"
+  },
+  {
+    "revision": "47dcd11188febd5aba8e",
+    "url": "/Whale-Finder/static/js/2.7f68fc25.chunk.js"
+  },
+  {
+    "revision": "db99e0b14166723c6d78",
+    "url": "/Whale-Finder/static/js/main.82a5ddc8.chunk.js"
+  },
+  {
+    "revision": "7cdabf836d279942bea2",
+    "url": "/Whale-Finder/static/js/runtime~main.1cee8ee6.js"
+  }
+]);
