f65f11dd94940a3240770cec9e48936c7d426bb9
EricNY <ericabt@gmail.com>
2019-09-05 13:24:16 -0400

    Updates

--- precache-manifest.4c378e03442018edd1aa24257da3b3ba.js
+++ precache-manifest.4c378e03442018edd1aa24257da3b3ba.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "10029f8fa88bc551dac184876d01e862",
+    "revision": "d7f504ea5f9a39013e22cf4624b60c6b",
     "url": "/Whale-Finder/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Whale-Finder/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "db99e0b14166723c6d78",
+    "revision": "43f4f41aaf64be2e5034",
     "url": "/Whale-Finder/static/css/main.e86ca9f3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Whale-Finder/static/js/2.7f68fc25.chunk.js"
   },
   {
-    "revision": "db99e0b14166723c6d78",
-    "url": "/Whale-Finder/static/js/main.82a5ddc8.chunk.js"
+    "revision": "43f4f41aaf64be2e5034",
+    "url": "/Whale-Finder/static/js/main.d3d886bd.chunk.js"
   },
   {
     "revision": "7cdabf836d279942bea2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Whale-Finder/precache-manifest.edd7f65ddcbd9ae6ef7f5e99326e524a.js"
+  "/Whale-Finder/precache-manifest.4c378e03442018edd1aa24257da3b3ba.js"
 );
 
 self.addEventListener('message', (event) => {
